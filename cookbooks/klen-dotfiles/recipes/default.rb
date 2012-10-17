2e1ec33b4cb46cd9d79c8f7cc2c34084d63911cb
Kirill Klenov <horneds@gmail.com>
2012-10-18 01:58:16 +0400

    fix

--- cookbooks/klen-dotfiles/recipes/default.rb
+++ cookbooks/klen-dotfiles/recipes/default.rb
@@ -13,10 +13,17 @@ execute "git clone #{node[:dotfiles][:source]} #{node[:dotfiles][:target]}" do
     not_if { File.directory?("#{node[:dotfiles][:target]}") }
 end
 
-['.bashrc', '.bash_profile', '.bash_aliases', '.gitconfig'].each do |file|
+['.bashrc', '.bash_profile', '.bash_aliases', '.gitconfig', '.ctags', '.hgrc', '.pylintrc', '.screenrc', 'bin', '.pip'].each do |file|
     link "#{node[:dotfiles][:home]}/#{file}" do
         to "#{node[:dotfiles][:target]}/#{file}"
         owner "#{node[:dotfiles][:user]}"
         group "#{node[:dotfiles][:user]}"
     end
+end
+
+execute "git clone #{node[:dotfiles][:vimsource]}" do
+    cwd "#{node[:dotfiles][:home]}"
+    user "#{node[:dotfiles][:user]}"
+    group "#{node[:dotfiles][:user]}"
+    not_if { File.directory?("#{node[:dotfiles][:home]}/.vim") }
 end
