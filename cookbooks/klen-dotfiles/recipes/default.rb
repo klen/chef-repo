c026fba8e954869605d0f114d5906446f3792f1b
Kirill Klenov <horneds@gmail.com>
2012-10-18 01:54:16 +0400

    fix

--- cookbooks/klen-dotfiles/recipes/default.rb
+++ cookbooks/klen-dotfiles/recipes/default.rb
@@ -13,20 +13,10 @@ execute "git clone #{node[:dotfiles][:source]} #{node[:dotfiles][:target]}" do
     not_if { File.directory?("#{node[:dotfiles][:target]}") }
 end
 
-link "#{node[:dotfiles][:home]}/.bashrc" do
-    to "#{node[:dotfiles][:target]}/.bashrc"
-    owner "#{node[:dotfiles][:user]}"
-    group "#{node[:dotfiles][:user]}"
-end
-
-link "#{node[:dotfiles][:home]}/.bash_aliases" do
-    to "#{node[:dotfiles][:target]}/.bash_aliases"
-    owner "#{node[:dotfiles][:user]}"
-    group "#{node[:dotfiles][:user]}"
-end
-
-link "#{node[:dotfiles][:home]}/.bash_profile" do
-    to "#{node[:dotfiles][:target]}/.bash_profile"
-    owner "#{node[:dotfiles][:user]}"
-    group "#{node[:dotfiles][:user]}"
+['.bashrc', '.bash_profile', '.bash_aliases', '.gitconfig'].each do |file|
+    link "#{node[:dotfiles][:home]}/#{file}" do
+        to "#{node[:dotfiles][:target]}/#{file}"
+        owner "#{node[:dotfiles][:user]}"
+        group "#{node[:dotfiles][:user]}"
+    end
 end
