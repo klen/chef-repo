0ad2fd3b0da877eaaf4b287fa5671326f0868ecf
Kirill Klenov <horneds@gmail.com>
2012-10-18 01:26:07 +0400

    update

--- cookbooks/klen-dotfiles/recipes/default.rb
+++ cookbooks/klen-dotfiles/recipes/default.rb
@@ -10,6 +10,7 @@ execute "git clone git://github.com/klen/.home.git" do
     cwd "/home/klen"
     user "klen"
     group "klen"
+    not_if { File.directory?("/home/klen/.home") }
 end
 
 execute "make install" do
