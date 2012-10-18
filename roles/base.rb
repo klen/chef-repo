92c14866894147d2cc4f0b4a7d3a61174d8ab834
Kirill Klenov <horneds@gmail.com>
2012-10-18 10:48:56 +0400

    Update base

--- roles/base.rb
+++ roles/base.rb
@@ -1,23 +1,26 @@
 name "base"
 description "Base role applied to all nodes."
 run_list(
-  "recipe[users::sysadmins]",
-  "recipe[sudo]",
-  "recipe[apt]",
-  "recipe[git]",
-  "recipe[build-essential]",
-  "recipe[logrotate]",
-  "recipe[locale-gen]",
-  "recipe[python]",
-  "recipe[curl]",
-  "recipe[nodejs]",
-  "recipe[vim]"
+    "recipe[users::sysadmins]",
+    "recipe[sudo]",
+    "recipe[apt]",
+    "recipe[git]",
+    "recipe[build-essential]",
+    "recipe[logrotate]",
+    "recipe[locale-gen]",
+    "recipe[python]",
+    "recipe[curl]",
+    "recipe[nodejs]",
+    "recipe[vim]"
 ) 
 override_attributes(
-  :authorization => {
-    :sudo => {
-      :users => ["ubuntu", "vagrant"],
-      :passwordless => true
+    :authorization => {
+        :sudo => {
+            :users => ["ubuntu", "vagrant"],
+            :passwordless => true
+        }
+    },
+    :localegen => {
+        :lang => ["en_US.UTF-8", "ru_RU.UTF-8"]
     }
-  }
 )
